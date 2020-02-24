b7f0b26821ec1c3ff99e021cf515f58e8437db9d
40000206 <40000206@BLTSP01453.utc.com>
2020-02-25 03:56:42 +0530

    changed directory for Hello world java file

--- Windchill_11/src/ext/test/HelloWorld.java
+++ Windchill_11/src/ext/test/HelloWorld.java
@@ -1,9 +1,10 @@
-package ext;
+package ext.test;
 
 public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("testedjsbfjs j");
+		System.out.println("Hello world");
 	}
 
 }
