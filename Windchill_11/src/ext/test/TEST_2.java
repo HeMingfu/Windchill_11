810f039a57fdab8ec5cb47e319a4ee39df11a38c
Mahesh <maheshawaji106@gmail.com>
2020-02-25 04:00:10 +0530

    added two test files

---
+++ Windchill_11/src/ext/test/TEST_2.java
@@ -0,0 +1,10 @@
+package ext.test;
+
+public class TEST_2 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello mic test from TEST_2");
+
+	}
+
+}
---
+++ Windchill_11/src/ext/test/TEST_NEW.java
@@ -0,0 +1,10 @@
+package ext.test;
+
+public class TEST_NEW {
+
+	public static void main(String[] args) {
+		System.out.println("Hello mic test");
+
+	}
+
+}
